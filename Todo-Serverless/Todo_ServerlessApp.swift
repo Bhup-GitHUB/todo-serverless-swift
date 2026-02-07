15636717a930edf5f2e3b33d77ad2705054ccfc8
Bhup-GitHUB <bkumar_be23@thapar.edu>
2026-02-08 03:17:12 +0530

    Initial Commit

---
+++ Todo-Serverless/Todo_ServerlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ServerlessApp.swift
+//  Todo-Serverless
+//
+//  Created by Bhupesh Kumar on 08/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ServerlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
